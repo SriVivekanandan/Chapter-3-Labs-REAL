06e768aa1229f82421738c56fc88db23cad41781
SriVivekanandan <70656799+SriVivekanandan@users.noreply.github.com>
2020-11-19 10:53:58 -0600

    .

--- Minivan.java
+++ Minivan.java
@@ -1,23 +1,25 @@
-
+import java.util.Scanner;
 /**
  * Write a description of class Minivan here.
  *
- * @author (your name)
+ * Sri Vivekanandan (your name)
  * @version (a version number or a date)
  */
 public class Minivan
 {
-    // instance variables - replace the example below with your own
-    private int x;
-
-    /**
-     * Constructor for objects of class Minivan
-     */
-    public Minivan()
-    {
-        // initialise instance variables
-        x = 0;
+    public static void main(String args[]){
+        Scanner input = new Scanner(System.in);
+        String number_code = new String();
+        
+        System.out.println("Please enter the code: ");
+        number_code = input.nextLine();
+        
+        //the gear letter will always be last in code
+        //so it is at 8 assuming the code length never changes
+        //0 for off      1 for on
+        
+        
+        
     }
-
     
 }
---
+++ MontyHall.java
@@ -0,0 +1,87 @@
+import java.util.Random;
+
+public class MontyHall
+{
+   public static void main(String[] args)
+
+   {
+
+      int strategy1Count = 0;
+      int strategy2Count = 0;
+      int NUMBER_OF_RUNS = 1000;
+
+      Random generator = new Random();
+
+ 
+
+      // Simulate the game 1000 times
+
+ 
+
+         // Host must pick a door that's not the player's door and not the
+         // door with the car
+
+           
+       //strategy2Count will count all the wins when not changing door
+
+        //strategy1Count will count all the wins when changing the door
+
+        
+
+         // player does not change door and sticks with picked door 
+
+         for(int i = 0; i <= NUMBER_OF_RUNS; i++){
+
+             int car_door = generator.nextInt(3);
+             int picked_door = generator.nextInt(3);
+             
+            if(car_door == picked_door){
+                strategy2Count = strategy2Count + 1;
+            }
+            }
+
+        
+
+         // player changes to the left over door after host picks goat door
+
+         for (int i = 0; i <= NUMBER_OF_RUNS; i++){
+            int car_door = generator.nextInt(3);
+            int picked_door = generator.nextInt(3);
+
+           
+            // find goat door for host to open that has not been picked by player and
+
+            // is not the car door
+
+            int host_goat_door = 0;
+
+           
+
+            while((host_goat_door == car_door ) || ( picked_door == host_goat_door)){
+                host_goat_door = generator.nextInt(3); //loop runs until host door is not a car door or a picked door
+            }
+
+            // remaining door
+
+            int other_door =  3 - (picked_door + host_goat_door);
+
+            // is remaining door car door
+
+            if(other_door == car_door){
+                strategy1Count++;
+            }
+            }
+
+        
+
+      System.out.printf("After %d runs, strategy 1 yielded %d wins while "
+            + "strategy 2 yielded %d.%n", NUMBER_OF_RUNS, strategy1Count,
+            strategy2Count);
+
+   }
+
+      
+
+}
+
+ 
